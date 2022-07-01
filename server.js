b08a4e5bbc93629b7bec51c93ef9a4797fd4e579
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-30 22:00:41 -0400

    added routes middleware to server.js; added route index.js to controllers and controllers/api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// TODO: Remember to add in middleware for routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
