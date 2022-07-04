eec493347e4e31c6551ea206918bb8945440bd3e
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-04 15:03:36 -0400

    Added express handlebars to package.json dependencies; created models for Comments, Posts, and Users; created associations for models in models/index.js

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'commenter_id',
+    onDelete: 'CASCADE'
+  });
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'commenter_id'
+});
+
+module.exports = { User, Post, Comment };
