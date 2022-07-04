1a9697732d1e41a272b1b48cf3aecad6d0e23483
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-04 19:03:32 -0400

    removed unnecessary commented out code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-// const { SECRET } = process.env;
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
