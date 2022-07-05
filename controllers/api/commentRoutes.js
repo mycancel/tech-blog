77a5bd9585e4c28032d68b22501a7745cefd2380
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-05 15:45:32 -0400

    changed user_id to commenter_id in commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -44,7 +44,7 @@ router.delete('/:id', async (req, res) => {
     const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        commenter_id: req.session.user_id,
       },
     });
 
