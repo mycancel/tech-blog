70bf37535cf7cf42e0a95de19776552bce3e8eb0
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-05 16:06:25 -0400

    added association between article and comments

--- models/index.js
+++ models/index.js
@@ -7,17 +7,26 @@ User.hasMany(Article, {
   onDelete: 'CASCADE'
 });
 
+Article.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 User.hasMany(Comment, {
     foreignKey: 'commenter_id',
     onDelete: 'CASCADE'
   });
 
-Article.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
 Comment.belongsTo(User, {
   foreignKey: 'commenter_id'
+});
+
+Article.hasMany(Comment, {
+  foreignKey: 'comment_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Article, {
+  foreignKey: 'comment_id'
 });
 
 module.exports = { Article, Comment, User };
