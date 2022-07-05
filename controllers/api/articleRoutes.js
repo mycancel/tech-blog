791101d3ec0a2308681e557f661308df29e6eda9
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-04 20:53:02 -0400

    changed datatype of content to TEXT; added post route to articleRoutes to create a new article

--- controllers/api/articleRoutes.js
+++ controllers/api/articleRoutes.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const { Article } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newArticle = await Article.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+
+    return res.status(200).json(newArticle);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
 
 module.exports = router;
--- models/Article.js
+++ models/Article.js
@@ -16,7 +16,7 @@ Article.init(
       allowNull: false,
     },
     content: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     date_created: {
       type: DataTypes.DATE,
