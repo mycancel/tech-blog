919a40751ace31069fd48ad30cdc69c55de2205b
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-04 22:45:43 -0400

    created home-routes.js; added get HTTP method to get all articles, serialize data, and render to the page

---
+++ controllers/home-route.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+// Get all articles
+router.get('/', async (req, res) => {
+  try {
+    const articleDbData = await Article.findAll();
+
+    // Serializes articles
+    const articles = articleDbData.map((article) =>
+      article.get({ plain: true })
+    );
+
+    // Renders articles to homepage
+    // TODO: create homepage.handlebars
+    res.render('homepage', {
+      articles,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// TODO: Login Redirect
+
+module.exports = router;
