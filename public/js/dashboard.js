e426b76975852ff9878d6fc0b51ce0ab19621900
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-05 18:20:30 -0400

    added form to dashboard-handlebars; added fetch request function to dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,27 @@
 const dashpage = async () => {
-    document.location.replace('/dashboard');
+  document.location.replace("/dashboard");
 };
 
-document.querySelector('#dashboard').addEventListener('click', dashpage);
+const createArticle = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const content = document.querySelector("#content").value;
+
+  console.log(title, content);
+
+  // Send fetch request to create a new article
+  const response = await fetch("api/articles", {
+    method: "POST",
+    body: JSON.stringify({ title, content }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to add article");
+  }
+};
+
+document.querySelector("#dashboard").addEventListener("click", dashpage);
+document.querySelector(".new-article-form").addEventListener("submit", createArticle);
