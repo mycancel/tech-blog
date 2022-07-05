3b5b11bd62da91bda9e7f5d380db25a2a254dd51
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-05 15:58:08 -0400

    created authentication utility and protected-routes file in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-route');
 const apiRoutes = require('./api');
+const protectedRoutes = require('./protected-routes');
+
+const withAuth = require('../utils/auth');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', withAuth, protectedRoutes);
 
 module.exports = router;
---
+++ controllers/protected-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+
+// Create routes to see Dashboard
+
+// Create routes to read articles
+// Comments related to articles will populate
+
+module.exports = router;
